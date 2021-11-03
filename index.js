4bcee249fa88e4b96b017f8a682e5e95e444df64
Doris Morales <moralesdoris880@gmail.com>
2021-11-03 13:56:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(drivers,name ="Bobby"){
+    const result = drivers.filter(names => names === name || names === name.toLowerCase() )
+    return result; 
+}
+const fuzzyMatch = (drivers,name) =>  drivers.filter(names => names.substr(0,name.length) === name)
+
+function matchName(drivers,name){
+    return drivers.filter(drivers => drivers.name == name);
+}
+
